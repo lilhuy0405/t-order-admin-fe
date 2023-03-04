ecfca814791f8f81f57cc0494b8e02212a331c0f
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-03-04 22:01:56 +0700

    change api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const TORDER_PROFILE = 'TORDER_PROFILE';
-// export const API_URL = 'https://torder-api.click';
+export const API_URL = 'https://torder-api.click';
 
-export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'http://localhost:8080';
