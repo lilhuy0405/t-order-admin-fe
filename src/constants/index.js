461c88480e678bdb5b7a0ac9ba1bd175b52d5c39
lilhuy0405 <luuhuy040501.travail@gmail.com>
2024-10-19 17:40:46 +0700

    update logic for JT

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,34 +6,39 @@ export const API_BASE_URL = 'https://torder-be.lilhuy-server.uk/';
 // export const API_URL = 'http://localhost:8080/api/v1';
 
 export const ORDER_FIELDS = [
-    {
-        name: 'customerName',
-        label: 'Customer Name',
-        required: true
-    },
-    {
-        name: 'phone',
-        label: 'Phone Number',
-        required: true
-    },
-    {
-        name: 'product',
-        label: 'Product',
-        required: true
-    },
-    {
-        name: 'shipCode',
-        label: 'Ship Code',
-        required: true
-    },
-    {
-        name: 'amount',
-        label: 'Amount',
-        required: false
-    },
-    {
-        name: 'shipAddress',
-        label: 'Ship Address',
-        required: false
-    }
+  {
+    name: 'customerName',
+    label: 'Customer Name',
+    required: false
+  },
+  {
+    name: 'phone',
+    label: 'Phone Number',
+    required: false
+  },
+  {
+    name: 'product',
+    label: 'Product',
+    required: true
+  },
+  {
+    name: 'shipCode',
+    label: 'Ship Code',
+    required: true
+  },
+  {
+    name: 'amount',
+    label: 'Amount',
+    required: false
+  },
+  {
+    name: 'shipAddress',
+    label: 'Ship Address',
+    required: false
+  },
+  {
+    name: 'jtCustomer',
+    label: 'JT Customer',
+    required: false
+  }
 ];
